fda15dffe551113059e5b428c71b4f5eee257336
Marian <marianstowerst@gmail.com>
2021-02-14 16:43:36 +0300

    Some Function

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -6,9 +6,24 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            int x1 = 4, y1 = 2;
+            char symb1 = '*';
 
+            Draw(x1, y1, symb1);
+
+            int x2 = 3, y2 = 1;
+            char symb2 = '-';
+
+            Draw(x2, y2, symb2);
+
+
+            Console.ReadLine();
+        }
+
+        static void Draw(int x,int y, char symb)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symb);
         }
     }
 }
